55bb703f05d569a0acd57dd46de8e0bd7fa11a0b
Hpekkaya <Ahpek1970@yahoo.com>
2024-03-22 09:09:04 +0300

    Create widgets and grid layout applied

--- main.py
+++ main.py
@@ -7,16 +7,36 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
-window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.title("Password Generator")
+window.config(padx=50, pady=50)
 
-canvas = Canvas()
+canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_image)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
+# --- Labels
 
+website_label = Label(width=20, text="Website :")
+username_label = Label(width=20, text="Email/Username :")
+password_label = Label(width=20, text="Password :")
+website_label.grid(row=1, column=0)
+username_label.grid(row=2, column=0)
+password_label.grid(row=3, column=0)
 
+# --- Entries
+website_entry = Entry(width=40)
+username_entry = Entry(width=40)
+password_entry = Entry(width=21)
+website_entry.grid(row=1, column=1, columnspan=2)
+username_entry.grid(row=2, column=1, columnspan=2)
+password_entry.grid(row=3, column=1)
+
+# --- Buttons
+password_button = Button(text="Generate Password")
+add_button = Button(width=36, text="Add")
+password_button.grid(row=3, column=2)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
 
